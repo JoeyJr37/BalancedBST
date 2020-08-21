85c23c226a10396bd64888bb0d004fa04484f24a
JoeyJr <JoeyJr11@gmail.com>
2020-08-21 14:31:39 -0500

    worked on binary search tree

--- bst.rb
+++ bst.rb
@@ -41,46 +41,130 @@ class Tree
             root_node
         end
     end
-    def test
-        p @root
-    end
-    # Write an #insert and #delete method which accepts a value to insert/delete 
-    # (you’ll have to deal with several cases for delete such as when a node has children or not). 
-    # If you need additional resources, check out these two articles on inserting and deleting, 
-    # or this video with several visual examples.
-    def internal_insert(root_node, value)
-        # if the value is smaller than the root node, go to left child
-        # if the value is larger than the root node, go to right child
-        # if the value is smaller than the root node and the left child is nil then make the value the left child
-        # if the value is larger than the root node and the right child is nil then make the value the right child
-        if root_node == nil
-            root_node = Node.new(value, nil, nil)
-        elsif root_node.value == value
-            puts "#{value} is already included in this tree."
-        elsif root_node.value > value
-            internal_insert(root_node.left_child, value)
-        elsif root_node.value < value
-            internal_insert(root_node.right_child, value)
+    def internal_print_tree(node, i)
+        if node == nil
+            nil
         else
-            puts "Something went wrong."
+            puts "Level #{i}: root: #{node.value}"
+            puts "Right_child: #{node.right_child.value}" unless node.right_child == nil 
+            puts "Left_child: #{node.left_child.value}" unless node.left_child == nil
+            if has_children?(node)
+                internal_print_tree(node.right_child, i + 1)
+                internal_print_tree(node.left_child, i + 1)
+            else
+                nil
+            end
         end
     end
 
-    def traverse_tree(root_node, new_node)
-        if root_node == nil
-            root_node = new_node
-        elsif root_node.value < new_node.value
-            traverse_tree(root_node.right_child, new_node)
-        elsif root_node.value > new_node.value
-            traverse_tree(root_node.left_child, new_node)
+    def print_tree
+        internal_print_tree(@root, 1)
+    end
+
+    # if the value is smaller than the root node, go to left child
+    # if the value is larger than the root node, go to right child
+    # if the value is smaller than the root node and the left child is nil then make the value the left child
+    # if the value is larger than the root node and the right child is nil then make the value the right child
+
+    def internal_insert(node, value)
+        if node.value == value
+            puts "#{value} is already included in the tree."
+        elsif node.value < value
+            if node.right_child == nil
+                node.right_child = Node.new(value, nil, nil)
+            else
+                internal_insert(node.right_child, value)
+            end
+        elsif node.value > value
+            if node.left_child == nil
+                node.left_child = Node.new(value, nil, nil)
+            else
+                internal_insert(node.left_child, value)
+            end
         else
             puts "Something went wrong!"
         end
     end
 
     def insert(value)
-        new_node = internal_insert(@root, value)
-        traverse_tree(@root, new_node)
-        p @root
+        internal_insert(@root, value)
+        print_tree
+    end
+            
+    def delete_a_leaf(node)
+        node = nil
+    end
+
+    def delete_node_with_one_child(parent_node, value)
+        if parent_node.left_child.value == value
+            parent_node.left_child = parent_node.left_child.left_child
+        elsif parent_node.right_child.value == value
+            parent_node.right_child = parent_node.right_child.left_child
+        else
+            puts "Something went wrong!"
+        end
+    end
+
+    def delete_node_with_two_children(node, value)
+        # node.right_child.left_child has_children? delete_node_with_two_children (node.right_child.left_child)
+        # once you get to final node.left_child save that value, then delete that leaf
+        # replace the value of original node with saved value of deleted leaf
+    end
+
+    def how_many_children(node)
+        counter = 0
+        if node.left_child != nil
+            counter += 1
+            if node.right_child != nil
+                counter += 1
+            end
+        end
+        counter
+    end
+
+    def delete_node(node, value)
+        if how_many_children(node) == 0
+            delete_a_leaf(node)
+        elsif how_many_children == 1
+            delete_node_with_one_child(node, value)
+        elsif how_many_children == 2
+            delete_node_with_two_children(node, value)
+        else
+            puts "Something went wrong!"
+        end
+    end
+
+    def delete(value)
+        # if the first node is equal to the submitted value, does it have children?
+        # if no then delete_a_leaf
+        # if one then delete_node_with_one_child
+        # if two then delete_node_with_two_children
+    end
+
+    def find_node(node,value)
+        # if node value == value, then return with that node
+        if node.value == value
+            node
+        elsif node.right_child != nil
+            if node.right_child.value == value
+                node.right_child
+            else
+                find_node(node.right_child, value)
+            end
+        elsif node.left_child != nil
+            if node.left_child.value == value
+                node.left_child
+            else
+                find_node(node.left_child, value)
+            end
+        else
+            puts "I can't find it!"
+        end
+    end
+
+    def print_children(value)
+        node = find_node(@root, value)
+        p node
+        # p how_many_children(node)
     end
 end
--- driver.rb
+++ driver.rb
@@ -1,4 +1,9 @@
 require_relative 'bst'
 
-test = Tree.new([2,3,4,3,5,2,1])
-test.insert(7)
+test = Tree.new([1,5,9,13,17,21,25,29,33,37])
+test.print_children(37)
+test.print_children(17)
+# test.print_children(13)
+# test.print_children(25)
+# test.print_children(29)
+
